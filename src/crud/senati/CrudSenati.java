a21423bedaf7f816e15c9ac89d82f803194f06ed
MARCO <decuxop@gmail.com>
2025-04-05 22:32:22 -0500

    Proyecto final

---
+++ src/crud/senati/CrudSenati.java
@@ -0,0 +1,17 @@
+package crud.senati;
+
+
+public class CrudSenati {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.println("hola mundo como estas");
+        Conexion cn = new Conexion();
+        
+        cn.conectar();
+    }
+    
+}
